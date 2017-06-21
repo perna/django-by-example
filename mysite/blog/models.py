bb52d46dae2577b214f7a313c3bb83094c15dc1c
Anderson Meira <andersonmeira@outlook.com>
2017-06-21 15:15:45 -0300

    implementação da lista de posts do blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
+
+    objects = models.Manager()
+    published = PublishedManager() 
+
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
@@ -22,3 +31,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                              self.publish.strftime('%m'),
+                              self.publish.strftime('%d'),
+                              self.slug])
